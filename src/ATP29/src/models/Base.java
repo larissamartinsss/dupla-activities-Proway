e645c58c616b2e11740bbfdffb6e3c652c47dcce
Larissa Martins <larissamartins.job@gmail.com>
2021-10-22 10:10:21 -0400

    CRUD finalizado! :D

--- src/ATP29/src/models/Base.java
+++ src/ATP29/src/models/Base.java
@@ -2,4 +2,5 @@ package models;
 
 public class Base {
     public int id;
+
 }
--- src/ATP29/src/models/Categorias.java
+++ src/ATP29/src/models/Categorias.java
@@ -1,12 +1,10 @@
 package models;
 
-public class Categorias {
+public abstract class Categorias extends Base {
 
     public String brinquedos;
     public String smartphone;
     public String computadores;
-
-
 
 
 }
